aboisvert/skiis

commit 8b36ff6748fc84efa9966d8e23484a13cf57ebd5

Eliminate synchronization bottleneck in FlatMapOp

src/main/scala/skiis/Skiis.scala
@@ -703,16 +703,20 @@ object Skiis {
 
     private[this] final val queue = new ArrayBuffer[U]()
 
-    override def next(): Option[U] = synchronized {
+    override def next(): Option[U] = {
       @tailrec def next0(): Option[U] = {
-        if (queue.size > 0) {
-          return Some(queue.remove(0))
+        synchronized {
+          if (queue.size > 0) {
+            return Some(queue.remove(0))
+          }
         }
         val next = previous.next()
         if (next == null || next.isEmpty) {
           None
         } else {
-          enqueue(next.get, queue += _)
+          synchronized {
+            enqueue(next.get, queue += _)
+          }
           next0()
         }
       }

commit d46a16eb8a7175921a403af0c05ce1609a9d8671

Fix concurrency bug in FlatMapOp.next() introduced in commit 8b36ff6 + add better test

src/main/scala/skiis/Skiis.scala
@@ -703,22 +703,44 @@ object Skiis {
 
     private[this] final val queue = new ArrayBuffer[U]()
 
+    private[this] val lock = new ReentrantLock()
+    private[this] var consumers = 0
+    private[this] val consuming = new Condition(lock)
+    private[this] var noMore = false
+
     override def next(): Option[U] = {
-      @tailrec def next0(): Option[U] = {
-        synchronized {
-          if (queue.size > 0) {
-            return Some(queue.remove(0))
-          }
-        }
+      while (true) {
+        lock.lock()
+        try {
+          if (queue.size > 0) return Some(queue.remove(0))
+          if (noMore && consumers == 0) return None
+          consumers += 1
+        } finally lock.unlock()
+
         val next = previous.next()
         if (next == null || next.isEmpty) {
-          None
+          lock.lock()
+          try {
+            consumers -= 1
+            noMore = true
+            consuming.signalAll()
+            if (consumers == 0) None
+            else consuming.await()
+          } finally lock.unlock()
         } else {
-          enqueue(next.get, x => synchronized { queue += x })
-          next0()
+          enqueue(next.get, { x =>
+            lock.lock()
+            try { queue += x }
+            finally lock.unlock()
+          })
+          lock.lock()
+          try {
+            consumers -= 1
+            consuming.signalAll()
+          } finally lock.unlock()
         }
       }
-      next0()
+      sys.error("unreachable")
     }
   }
 

src/test/scala/skiis/ParMapSuite.scala
@@ -1,0 +1,64 @@
+package skiis
+
+import java.util.concurrent.Executors
+import org.scalatest.WordSpec
+import org.scalatest.matchers.ShouldMatchers
+import scala.collection._
+import java.util.concurrent.atomic.AtomicInteger
+
+@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
+class ParMapSuite extends WordSpec with ShouldMatchers {
+  import Skiis._
+
+  val r = new scala.util.Random
+
+  "Skiis" should {
+    // implicit val context = Skiis.DefaultContext
+
+
+    for (i <- 1 to 10) {
+      
+      implicit val context = new Skiis.Context {
+        override lazy val parallelism = r.nextInt(8) + 8 
+        override lazy val queue = r.nextInt(100) + 1
+        override lazy val batch = r.nextInt(10) + 1
+        override lazy val executor = Executors.newFixedThreadPool(
+          /* threads */ parallelism + 1,
+          daemonThreadFactory("ParMapSuite")
+        )
+      }
+      
+      ("parMap %d" format i) in {
+        val acc1 = new AtomicInteger()
+        val acc2 = new AtomicInteger()
+        val acc3 = new AtomicInteger()
+        val acc4 = new AtomicInteger()
+
+        val skiis1 = Skiis(1 to 1000 grouped 25) flatMap { batch =>
+          acc1.incrementAndGet()
+          Thread.sleep(r.nextInt(50))
+          val b = batch map { x => (x, x + 1) }
+          Skiis(b)
+        }
+
+        val skiis = skiis1 parMap { case (x1, x2) =>
+          acc2.incrementAndGet()
+          Thread.sleep(r.nextInt(50))
+          (x1, x2, x1)
+        }
+
+        skiis foreach { case (x1, x2, x3) =>
+          x2 should be === (x1 + 1) 
+          x3 should be === (x1) 
+          acc3.incrementAndGet()
+          acc4.addAndGet(x1)
+        }
+
+        acc1.get should be === 40
+        acc2.get should be === 1000
+        acc3.get should be === 1000
+        acc4.get should be === (1 to 1000).sum
+      }
+    }
+  }
+}